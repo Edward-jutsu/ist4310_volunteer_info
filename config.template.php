ef4a3deb7e2c4f206691c287d376c641da5d7cae
Edward-jutsu <105586180+Edward-jutsu@users.noreply.github.com>
2022-05-15 03:06:38 -0700

    Initial commit of project files

---
+++ config.template.php
@@ -0,0 +1,7 @@
+<?php 
+// copy this config file to config.php and enter your connection settings
+
+define("DB_HOST", "127.0.0.1");
+define("DB_NAME", "ist4310_volunteer_info");
+define("DB_USER", "ist4310_volunteer_info_user");
+define("DB_PASSWORD", "xxxxx");
